f4e9d18fb8148bdc72e66b9f6f6a968cb73cd139
Xiaosea <maysrp@gmail.com>
2020-04-21 21:37:40 +0800

    badapple

---
+++ boot.py
@@ -0,0 +1,16 @@
+from machine import I2C,Pin
+import time
+import uos
+import json
+i2c=I2C(sda=Pin(5), scl=Pin(4), freq=40000000)
+from ssd1306 import SSD1306_I2C
+oled = SSD1306_I2C(128, 64, i2c) 
+def image(img_list):   
+    oled.fill(0)     
+    for i in img_list:
+        oled.hline(2*i[0],2*i[1],2*i[2],1)
+    oled.show()
+with open('bad.data','r') as f:
+    for i in f:
+        z=json.loads(i)
+        image(z)
