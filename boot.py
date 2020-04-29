5fd05db868915f3ea25a7165b8bf8a79c4f01fe2
Xiaosea <maysrp@gmail.com>
2020-04-29 21:08:20 +0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,16 +1,25 @@
 from machine import I2C,Pin
-import time
+import gc
 import uos
+import time
 import json
 i2c=I2C(sda=Pin(5), scl=Pin(4), freq=40000000)
 from ssd1306 import SSD1306_I2C
 oled = SSD1306_I2C(128, 64, i2c) 
-def image(img_list):   
+def image(img_list):
+    s=time.ticks_ms()   
     oled.fill(0)     
     for i in img_list:
-        oled.hline(2*i[0],2*i[1],2*i[2],1)
+        oled.hline(i[0],i[1],i[2],1)
     oled.show()
+    e=time.ticks_ms()-s
+    if e<100:
+        time.sleep_ms(90-e)
 with open('bad.data','r') as f:
     for i in f:
-        z=json.loads(i)
-        image(z)
+        try:
+            z=json.loads(i)
+            image(z)
+        except Exception as e:
+            pass
+        gc.collect()
